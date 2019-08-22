0f021de2327a9532357695f553fce87560556e16
김민수 <alstn2468_@naver.com>
2019-08-22 23:55:16 +0900

    Set all navigationBar

--- nomad-movies/App.js
+++ nomad-movies/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import * as Font from "expo-font";
 import { AppLoading } from "expo";
 import { Ionicons } from "@expo/vector-icons";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "react-native";
 import MainNavigation from "./navigation/MainNavigation";
 
 export default class App extends React.Component {
@@ -24,7 +24,12 @@ export default class App extends React.Component {
     const { loaded } = this.state;
 
     if (loaded) {
-      return <MainNavigation />;
+      return (
+        <>
+          <StatusBar barStyle="light-content" />
+          <MainNavigation />
+        </>
+      );
     } else {
       return (
         <AppLoading
@@ -36,12 +41,3 @@ export default class App extends React.Component {
     }
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
