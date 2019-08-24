e60caf1b47177e159dc9008b2f27e584ecbe0406
김민수 <alstn2468_@naver.com>
2019-08-24 16:07:38 +0900

    Add Loader.js for Loading Component

--- nomad-movies/screens/Movies/MoviesContainer.js
+++ nomad-movies/screens/Movies/MoviesContainer.js
@@ -5,6 +5,7 @@ export default class extends React.Component {
     state = {
         loading: true
     };
+
     render() {
         const { loading } = this.state;
         return <MoviesPresenter loading={loading} />;
