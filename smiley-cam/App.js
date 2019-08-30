955e4077fdc13cb6c20dfc7fc657aca8b3b1d01f
김민수 <alstn2468_@naver.com>
2019-08-30 21:01:57 +0900

    Get camera permission using expo-permission

--- smiley-cam/App.js
+++ smiley-cam/App.js
@@ -1,19 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import * as Permissions from "expo-permissions";
+import { Camera } from "expo-camera";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+    state = {
+        hasPermission: null
+    };
+
+    componentDidMount = async () => {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA);
+
+        if (status === "granted") {
+            this.setState({ hasPermission: true });
+        } else {
+            this.setState({ hasPermission: false });
+        }
+    };
+
+    render() {
+        const { hasPermission } = this.state;
+
+        if (hasPermission) {
+            return (
+                <View>
+                    <Text>Has Permission</Text>
+                </View>
+            );
+        } else if (hasPermission === false) {
+            return (
+                <View>
+                    <Text>Don't have Permission for this App.</Text>
+                </View>
+            );
+        } else {
+            return <ActivityIndicator />;
+        }
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
