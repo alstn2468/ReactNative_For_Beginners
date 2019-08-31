7e5292d387ea4e7fa155b6d2723f1734319a3240
김민수 <alstn2468_@naver.com>
2019-09-01 00:40:46 +0900

    Complete smiley cam application

--- smiley-cam/App.js
+++ smiley-cam/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { ActivityIndicator, Dimensions, TouchableOpacity } from "react-native";
-import * as Permissions from "expo-permissions";
 import { MaterialIcons } from "@expo/vector-icons";
 import { Camera } from "expo-camera";
+import * as Permissions from "expo-permissions";
+import * as FaceDetector from "expo-face-detector";
+import * as MediaLibrary from "expo-media-library";
 import styled from "styled-components";
 
 const { width, height } = Dimensions.get("window");
+
+const ALBUM_NAME = "Smiley Cam";
 
 const CenterView = styled.View`
     flex: 1;
@@ -24,10 +28,17 @@ const IconBar = styled.View`
 `;
 
 export default class App extends React.Component {
-    state = {
-        hasPermission: null,
-        cameraType: Camera.Constants.Type.front
-    };
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hasPermission: null,
+            cameraType: Camera.Constants.Type.front,
+            smileDetected: false
+        };
+
+        this.cameraRef = React.createRef();
+    }
 
     componentDidMount = async () => {
         const { status } = await Permissions.askAsync(Permissions.CAMERA);
@@ -39,7 +50,7 @@ export default class App extends React.Component {
     };
 
     render() {
-        const { hasPermission, cameraType } = this.state;
+        const { hasPermission, cameraType, smileDetected } = this.state;
 
         if (hasPermission === true) {
             return (
@@ -52,6 +63,17 @@ export default class App extends React.Component {
                             overflow: "hidden"
                         }}
                         type={cameraType}
+                        onFacesDetected={
+                            smileDetected ? null : this.onFacesDetected
+                        }
+                        faceDetectionClassifications="all"
+                        faceDetectorSettings={{
+                            detectLandmarks:
+                                FaceDetector.Constants.Landmarks.all,
+                            runClassifications:
+                                FaceDetector.Constants.Classifications.all
+                        }}
+                        ref={this.cameraRef}
                     />
                     <IconBar>
                         <TouchableOpacity onPress={this.switchCameraType}>
@@ -94,6 +116,76 @@ export default class App extends React.Component {
             this.setState({
                 cameraType: Camera.Constants.Type.front
             });
+        }
+    };
+
+    onFacesDetected = faces => {
+        const {
+            faces: [face]
+        } = faces;
+
+        if (face) {
+            console.log("smilingProbability :", face.smilingProbability);
+
+            if (face.smilingProbability > 0.7) {
+                this.setState({
+                    smileDetected: true
+                });
+                this.takePhoto();
+            }
+        }
+    };
+
+    takePhoto = async () => {
+        try {
+            if (this.cameraRef.current) {
+                let { uri } = await this.cameraRef.current.takePictureAsync({
+                    quality: 1
+                });
+
+                if (uri) {
+                    this.savePhoto(uri);
+                }
+            }
+        } catch (error) {
+            alert(error);
+
+            this.setState({
+                smileDetected: false
+            });
+        }
+    };
+
+    savePhoto = async uri => {
+        try {
+            const { status } = await Permissions.askAsync(
+                Permissions.CAMERA_ROLL
+            );
+
+            if (status === "granted") {
+                const asset = await MediaLibrary.createAssetAsync(uri);
+                let album = await MediaLibrary.getAlbumAsync(ALBUM_NAME);
+
+                if (album === null) {
+                    album = await MediaLibrary.createAlbumAsync(
+                        ALBUM_NAME,
+                        asset
+                    );
+                } else {
+                    await MediaLibrary.addAssetsToAlbumAsync([asset], album.id);
+                }
+                setTimeout(
+                    () =>
+                        this.setState({
+                            smileDetected: false
+                        }),
+                    2000
+                );
+            } else {
+                this.setState({ hasPermission: false });
+            }
+        } catch (error) {
+            console.log(error);
         }
     };
 }
